b7ed6e223f770247ebf77d72b373f392902d36d3
Jacoby <jacobmadestuff@gmail.com>
2018-03-04 16:39:54 -0800

    Ui layout set up

--- ui.R
+++ ui.R
@@ -10,24 +10,19 @@
 library(shiny)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+shinyUI(fluidPage((tabsetPanel(
+    tabPanel("Introduction",
+           "Test1"   
     ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
+    tabPanel("Gender vs Earnings",
+    "Test2"  
+    ),
+    tabPanel("Race vs Earnings",
+          "Test3"
+    ),
+    tabPanel("Number of Biopics by Race",
+             "Test4"
     )
+  )
   )
 ))
