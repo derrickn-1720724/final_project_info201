daa41363fa7ae179498086eb87f80f3ade6f70aa
derrickn-1720724 <32498758+derrickn-1720724@users.noreply.github.com>
2018-03-06 15:43:53 -0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,11 +1,9 @@
 library(shiny)
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage((tabsetPanel(
   
   tabPanel("Introduction",
            "Test1"   
-<<<<<<< HEAD
   ),
   
   tabPanel("Gender vs Earnings",
@@ -27,23 +25,6 @@ shinyUI(fluidPage((tabsetPanel(
   
   tabPanel("Number of Biopics by Race",
            "Test4"
-=======
-    ),
-    tabPanel("Gender vs Earnings",
-    "Test2"  
-    ),
-    tabPanel("Race vs Earnings",
-          "Test3",
-          selectInput("race",unique(race$subject_race), label = "Subject's Race"),
-          radioButtons("year",c("before 2000","after 2000"),label = "Year released"),
-          plotOutput("raceGraph")
-          
-    ),
-    tabPanel("Number of Biopics by Race",
-             "Test4"
-    )
-  )
->>>>>>> 5dc259817a4a2bf3386fe701997a52d2b5e7983e
   )
 )
 )
